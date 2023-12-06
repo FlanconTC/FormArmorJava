4c87b3635062d661c015265e0cde2562fff83d09
FlanconTC <corentinblg@yahoo.com>
2023-12-06 11:28:18 +0100

    Requêtes préparées

--- src/modele/GestionSql.java
+++ src/modele/GestionSql.java
@@ -1,5 +1,6 @@
 package modele;
-
+import java.sql.*;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -14,7 +15,8 @@ public class GestionSql
 {
     static String pilote = "com.mysql.cj.jdbc.Driver";
     static String url = new String("jdbc:mysql://localhost/symfony6_formarmor?characterEncoding=UTF8");
-    //Requete permettant de retourner l'ensemble des clients
+
+    //OK NIVEAU PARAM
     public static ObservableList<Client> getLesClients()
     {
         Connection conn;
@@ -27,9 +29,11 @@ public class GestionSql
             conn = DriverManager.getConnection(url,"root","");
             stmt1 = conn.createStatement();
             
-            // Liste des clients qui "ont un plan de formation"
+            //REQ
             String req = "select distinct c.id, statut_id, username, password, adresse, cp, ville, email, nbhcompta, nbhbur from client c, plan_formation p "
             + "where c.id = p.client_id order by c.id";
+            
+            
             ResultSet rs = stmt1.executeQuery(req);
             while (rs.next())
             {
@@ -48,27 +52,28 @@ public class GestionSql
         return lesClients;
     }
     
-    //Requête permettant de  retourner les sessions autorisées pour le client sélectionné
+    //OK NIVEAU PARAM
     public static ObservableList<Session> getLesSessions(int client_id)
     {
         Connection conn;
-        Statement stmt1;
         Session maSession;
         ObservableList<Session> lesSessions = FXCollections.observableArrayList();
         try
         {
-            // On prévoit 2 connexions à la base
-
             Class.forName(pilote);
             conn = DriverManager.getConnection(url,"root","");
-            stmt1 = conn.createStatement();
             
-            // Sélection des sessions autorisées pour le client choisi
-            String req = "call sessions_autorisees('" + client_id + "')";
-            ResultSet rs = stmt1.executeQuery(req);
+            
+            //REQ
+            PreparedStatement stmt1 = conn.prepareStatement
+            ("call sessions_autorisees(?)");         
+            //AJOUT DU PARAMETRE
+            stmt1.setInt(1, client_id); 
+            
+            
+            ResultSet rs = stmt1.executeQuery();
             while (rs.next())
             {
-                // A MODIFIER
                 maSession = new Session(rs.getInt("id"), rs.getString("libelle"), rs.getDate("date_debut"), rs.getInt("nb_places"), rs.getInt("nb_inscrits"));
                 lesSessions.add(maSession);
             }
@@ -84,34 +89,31 @@ public class GestionSql
         return lesSessions;
     }
     
-    //Requête permettant l'insertion de l'inscription dans la table inscription et
-    //la mise à jour de la table session_formation (+1 inscrit) et
-    //la mise à jour de la table plan_formation (effectue passe à 1)
+    //OK NIVEAU PARAM
     public static boolean insereInscription(int matricule, int session_formation_id)
     {
-        Connection conn;
-        Statement stmt2=null;
+        int numForm = 0;
         String present = "Present";
         GregorianCalendar dateJour = new GregorianCalendar();
         String ddate = dateJour.get(GregorianCalendar.YEAR) + "-" + (dateJour.get(GregorianCalendar.MONTH) + 1) + "-" + dateJour.get(GregorianCalendar.DATE);
-        // Insertion dans la table inscription
-        String req = "Insert into inscription(client_id, sessionformation_id, date_inscription, presence) values (" + matricule;
-        req += ", " + session_formation_id + ",'" + ddate + "','" + present + "')";
-        // M.A.J de la table session_formation (un inscrit de plus)
-        String req2 = "Update session_formation set nb_inscrits = nb_inscrits +1 Where id = " + session_formation_id;
-        // Récupération du numéro de la session concernée
-        String req3 = "Select formation_id from session_formation where id = " + session_formation_id;
-        //stmt1 = GestionBdd.connexionBdd(GestionBdd.TYPE_MYSQL, "symfony6_formarmor", "localhost", "root", "");
-        int numForm=0;
         try
         {
-            Class.forName(pilote);
+            Connection conn;
             conn = DriverManager.getConnection(url,"root","");
-            stmt2 = conn.createStatement();
+          
+            
+            //REQ 3
+            PreparedStatement stmt3 = conn.prepareStatement
+            ("Select formation_id from session_formation where id = ?;");         
+            //AJOUT DU PARAMETRE
+            stmt3.setInt(1, session_formation_id); 
+
+            
+            Class.forName(pilote);
             System.out.println("EXECUTION REQ3 : ");
-            ResultSet rs = stmt2.executeQuery(req3);
+            ResultSet rs = stmt3.executeQuery();
             rs.next();
-            numForm = rs.getInt(1);
+            numForm = rs.getInt(1);      
         }
         catch (ClassNotFoundException cnfe)
         {
@@ -122,53 +124,79 @@ public class GestionSql
         {
             System.out.println("Erreur requete insereInscription " + e.getMessage());
             return false;
-        }
-        // M.A.J de la table plan_formation (effectue passe à 1 pour le client et la session concernés)
-        String req4 = "Update plan_formation set effectue = 1 Where client_id = " + matricule;
-        req4 += " And formation_id = " + numForm;
+        }      
         try
         {
-            System.out.println("EXECUTION REQ : ");
-            int nb1 = stmt2.executeUpdate(req);
-            System.out.println("EXECUTION REQ2 : ");
-            int nb2 = stmt2.executeUpdate(req2);
-            System.out.println("EXECUTION REQ4 : ");
-            int nb3 = stmt2.executeUpdate(req4);
+            Connection conn;
+            conn = DriverManager.getConnection(url,"root","");
+            
+            
+            //REQ 1
+            PreparedStatement stmt1 = conn.prepareStatement
+            ("Insert into inscription(client_id, sessionformation_id, date_inscription, presence) values ( ?, ?, ?, ?);");            
+            //AJOUT DES PARAMETRES
+            stmt1.setInt(1, matricule); 
+            stmt1.setInt(2, session_formation_id);  
+            stmt1.setString(3, ddate); 
+            stmt1.setString(4, present); 
+           
+
+            //REQ 2
+            PreparedStatement stmt2 = conn.prepareStatement
+            ("Update session_formation set nb_inscrits = nb_inscrits +1 Where id = ?;");         
+            //AJOUT DU PARAMETRE
+            stmt2.setInt(1, session_formation_id); 
+
+            
+            //REQ 4
+            PreparedStatement stmt4 = conn.prepareStatement
+            ("Update plan_formation set effectue = 1 Where client_id = ? And formation_id = ?;");         
+            //AJOUT DU PARAMETRE
+            stmt4.setInt(1, matricule); 
+            stmt4.setInt(2, numForm);
+            
+            
+            System.out.println("EXECUTION REQ 1: ");
+            stmt1.executeUpdate();
+            System.out.println("EXECUTION REQ 2: ");
+            stmt2.executeUpdate();
+            System.out.println("EXECUTION REQ 4: ");
+            stmt4.executeUpdate();
         }
         catch(SQLException sqle)
         {
-            
             System.out.println("Erreur SQL maj des inscriptions : " + sqle.getMessage());
             return false;
         }
         return true;
     }
     
-    
-    
-      public static ObservableList<Session> getLesSessionsFinies()
+    //OK NIVEAU PARAM
+    public static ObservableList<Session> getLesSessionsFinies()
     {
         Connection conn;
-        Statement stmt1;
         Session maSession;
         ObservableList<Session> lesSessions = FXCollections.observableArrayList();
         try
         {
-            // On prévoit 2 connexions à la base
-
             Class.forName(pilote);
             conn = DriverManager.getConnection(url,"root","");
-            stmt1 = conn.createStatement();
-            LocalDate dateDuJour = LocalDate.now();
-            // Sélection des sessions autorisées pour le client choisi
-            String req = "select s.id, f.libelle, s.date_debut, s.nb_places, s.nb_inscrits ";
-            req += "from session_formation s, formation f ";
-            req += "where s.formation_id = f.id ";
-            req += "and s.date_debut <'"+ dateDuJour+"'";
-            ResultSet rs = stmt1.executeQuery(req);
+            Date dateDuJour = Date.valueOf(LocalDate.now());
+           
+            
+            //REQ
+            PreparedStatement stmt1 = conn.prepareStatement
+            ("select s.id, f.libelle, s.date_debut, s.nb_places, s.nb_inscrits "
+            + "from session_formation s, formation f "
+            + "where s.formation_id = f.id "
+            + "and s.date_debut < ?");
+            //AJOUT DU PARAMETRE
+            stmt1.setDate(1, dateDuJour);
+        
+            
+            ResultSet rs = stmt1.executeQuery();
             while (rs.next())
             {
-                // A MODIFIER
                 maSession = new Session(rs.getInt("id"), rs.getString("libelle"), rs.getDate("date_debut"), rs.getInt("nb_places"), rs.getInt("nb_inscrits"));
                 lesSessions.add(maSession);
             }
@@ -183,27 +211,32 @@ public class GestionSql
         }
         return lesSessions;
     }
-      
-     public static ObservableList<Client> getLesClientsInscrits(int id)
+    
+    //OK NIVEAU PARAM
+    public static ObservableList<Client> getLesClientsInscrits(int id)
     {
         Connection conn;
-        Statement stmt1;
         Client monClient;
         ObservableList<Client> lesClients = FXCollections.observableArrayList();
         try
         {
             Class.forName(pilote);
             conn = DriverManager.getConnection(url,"root","");
-            stmt1 = conn.createStatement();
             
-            // Liste des clients qui "ont un plan de formation"
-            String req = "SELECT c.*, i.presence \n" +
+            
+            //REQ
+            PreparedStatement stmt1 = conn.prepareStatement
+            ("SELECT c.*, i.presence \n" +
             " FROM client c\n" +
             " JOIN inscription i ON c.id = i.client_id\n" +
             " JOIN session_formation sf ON i.sessionformation_id = sf.id\n" +
             " WHERE i.sessionformation_id = sf.id\n" +
-            " AND sf.id = '" + id + "'";
-            ResultSet rs = stmt1.executeQuery(req);
+            " AND sf.id = ?");
+            //AJOUT DU PARAMETRE
+            stmt1.setInt(1, id);
+            
+            
+            ResultSet rs = stmt1.executeQuery();
             while (rs.next())
             {
                 monClient = new Client(rs.getInt("id"), rs.getInt("statut_id"), rs.getInt("nbhcompta"), rs.getInt("nbhbur"), rs.getString("username"), rs.getString("password"), rs.getString("adresse"), rs.getString("cp"), rs.getString("ville"), rs.getString("email"), rs.getString("presence"));
@@ -221,24 +254,29 @@ public class GestionSql
         return lesClients;
     }
      
-     
-      public static void setUnClientAbsent(int id, int formation, String presence)
+    //OK NIVEAU PARAM
+    public static void setUnClientAbsent(int id, int formation, String presence)
     {
         Connection conn;
-        Statement stmt1;
         try
         {
             Class.forName(pilote);
             conn = DriverManager.getConnection(url,"root","");
-            stmt1 = conn.createStatement();
             
-       
-            String req = "Update inscription" +
-            " Set presence = '"+ presence +
-            "' Where client_id = '"+ id +
-            "' and sessionformation_id = '"+ formation +"';";
-            stmt1.executeUpdate(req);
-         
+            
+            //REQ
+            PreparedStatement stmt1 = conn.prepareStatement
+            ("Update inscription" +
+            " Set presence = ?" +
+            " Where client_id = ?"+
+            " and sessionformation_id = ?;");
+            //AJOUT DES PARAMETREs
+            stmt1.setString(1, presence);
+            stmt1.setInt(2, id);
+            stmt1.setInt(3, formation);
+
+            
+            stmt1.executeUpdate();
         }
         catch (ClassNotFoundException cnfe)
         {
@@ -250,27 +288,32 @@ public class GestionSql
         } 
     }
       
-      
-       public static float getMarge(int id)
+    //OK NIVEAU PARAM
+    public static float getMarge(int id)
     {
         Connection conn;
-        Statement stmt1;
         float laMarge = 0;
         try
         {
             Class.forName(pilote);
             conn = DriverManager.getConnection(url,"root","");
-            stmt1 = conn.createStatement();
             
-            String req = "SELECT  DISTINCT(c.id), s.taux_horaire  \n" +
-            " from statut s, client c, formation f  \n" +
-            " where c.statut_id = s.id and c.id in  \n" +
+            
+            //REQ
+            PreparedStatement stmt1 = conn.prepareStatement
+            ("SELECT  DISTINCT(c.id), s.taux_horaire  \n" +
+                " from statut s, client c, formation f  \n" +
+                " where c.statut_id = s.id and c.id in  \n" +
                 " (SELECT DISTINCT c.id  \n" +
                 " FROM client c  \n" +
                 " JOIN inscription i ON c.id = i.client_id \n" +
                 " JOIN session_formation sf ON i.sessionformation_id = sf.id  \n" +
-                " WHERE sf.id = '"+ id +"');";
-            ResultSet rs = stmt1.executeQuery(req);
+                " WHERE sf.id = ?);");
+            //AJOUT DU PARAMETRE
+            stmt1.setInt(1, id);
+            
+                
+            ResultSet rs = stmt1.executeQuery();
             while (rs.next())
             {
                 laMarge += rs.getFloat("taux_horaire");
@@ -287,25 +330,28 @@ public class GestionSql
         return laMarge;
     }
        
-       
-        public static float getPrixSession(int id)
+    //OK NIVEAU PARAM 
+    public static float getPrixSession(int id)
     {
         Connection conn;
-        Statement stmt1;
-       
         float prix = 0;
         try
         {
             Class.forName(pilote);
             conn = DriverManager.getConnection(url,"root","");
-            stmt1 = conn.createStatement();
             
-            // Liste des clients qui "ont un plan de formation"
-            String req = "SELECT f.coutrevient\n" +
+            
+            //REQ
+            PreparedStatement stmt1 = conn.prepareStatement
+            ("SELECT f.coutrevient\n" +
             " FROM formation f, session_formation sf \n" +
-            " WHERE sf.id = '"+id+"'" +
-            " and f.id = sf.formation_id;";
-            ResultSet rs = stmt1.executeQuery(req);
+            " WHERE sf.id = ?" +
+            " and f.id = sf.formation_id;");
+            //AJOUT DU PARAMETRE
+            stmt1.setInt(1, id);
+            
+            
+            ResultSet rs = stmt1.executeQuery();
             while (rs.next())
             {
                 prix = rs.getFloat("coutrevient");
